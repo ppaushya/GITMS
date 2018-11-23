0774862c3d94212004d89f7db7edff6f551b170b
mohsinha <mohsinha@DIN73002729.corp.capgemini.com>
2018-11-23 14:24:29 +0530

    uploading to git

---
+++ GitDemo/src/main/java/TestClass.java
@@ -0,0 +1,16 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First");
+		
+		method1();
+
+	}
+  public static void method1() {
+	  System.out.println("method 1 statement");
+	  
+  }
+}
